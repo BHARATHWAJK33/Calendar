09883470771460d33368d4ff2fdfbc576a222ecf
BHARATHWAJK33 <bharathwaj6366@gmail.com>
2025-08-06 20:44:21 +0530

    Day 1

---
+++ Calendar/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import leftarrow from './assets/leftarrow.svg'
+import rightarrow from './assets/rightarrow.svg'
+function App() {
+
+  return (
+    <>
+    <div className="Calendar">
+      <div className="header">
+        
+        
+        <button><img src={leftarrow} /></button>
+        <select name="" id=""></select>
+        <select name="" id=""></select>
+        <button><img src={rightarrow}  /></button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
